e269f4d841312ce6267fb6dc76806fe4ee87fb48
Danylo2077 <123510448+Danylo2077@users.noreply.github.com>
2023-09-14 18:26:09 +0300

    Add files via upload

---
+++ Lab_1/Lab#1.sql
@@ -0,0 +1,20 @@
+1)Select *
+from cities
+Order by population DESC
+Limit 10 offset 10;
+
+2)Select *
+from cities
+Order by name Desc
+Limit 30;
+
+3)Select *
+from cities
+Order by region, population Desc;
+
+4)Select Distinct region
+from cities;
+
+5)Select *
+from cities
+Order by region Desc, name Desc;
