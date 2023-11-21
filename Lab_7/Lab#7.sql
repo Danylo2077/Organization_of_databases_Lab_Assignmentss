f9a08c8f18c4ea69582d2f0c191cdfb8ed99ab0e
Danylo2077 <123510448+Danylo2077@users.noreply.github.com>
2023-11-21 23:47:48 +0200

    Add files via upload

--- Lab_7/Lab#7.sql
+++ Lab_7/Lab#7.sql
@@ -1,38 +1,43 @@
+
 // Таблиця Categories 
+//Тут можна зберігати дані про категорії новин
 
 CREATE TABLE Categories (
-    category_id int AUTO_INCREMENT PRIMARY KEY,
-    category_name  varchar(255) not null
+    id int AUTO_INCREMENT PRIMARY KEY,
+    name  varchar(255) not null
 );
 
  //Таблиця News
+ //Тут можна зберігати дані про новини, пов'язані з категоріями
 
 CREATE TABLE News (
-    news_id int AUTO_INCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     category_id int,
     title varchar(255) not null,
     content text not null,
     publication_date datetime not null,
-    FOREIGN KEY (category_id) REFERENCES Categories(category_id)
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
 );
 
 //Таблиця Comments 
+//Тут можна зберігати коментарі до новин
 
 CREATE TABLE Comments (
-    comment_id int AUTO_INCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     news_id int,
     commenter_name  varchar(255) not null,
     comment_text text not null,
-    comment_date datetime not null,
-    FOREIGN KEY (news_id) REFERENCES News(news_id)
+    date datetime not null,
+    FOREIGN KEY (news_id) REFERENCES News(id)
 );
 
 //Таблиця Ratings 
+//Тут можна зберігати рейтинги новин з визначеними значеннями від 1 до 5
 
 CREATE TABLE Ratings (
-    rating_id int AUTO_INCREMENT PRIMARY KEY,
+    id int AUTO_INCREMENT PRIMARY KEY,
     news_id int,
     ip_address  varchar(255) not null,
     rating_value int CHECK (rating_value >= 1 AND rating_value <= 5) not null,
-    FOREIGN KEY (news_id) REFERENCES News(news_id)
+    FOREIGN KEY (news_id) REFERENCES News(id)
 );
