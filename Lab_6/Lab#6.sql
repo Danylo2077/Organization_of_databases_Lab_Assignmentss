b581d570d7ae6d269bc5ab5f69c05ccdb62a2489
Danylo2077 <123510448+Danylo2077@users.noreply.github.com>
2023-11-22 09:56:57 +0200

    Add files via upload

--- Lab_6/Lab#6.sql
+++ Lab_6/Lab#6.sql
@@ -9,53 +9,61 @@ Join regions on cities.region = regions.uuid
 Where regions.name = 'Nord';
 
 
-3) //Прибрав таблицю Cities
+3) 
 
-
-
-// Таблиця Lines (Ця таблиця містить інформацію про різні лінії метро в місті)
-                                                                            
-CREATE TABLE Lines (
+// Таблиця lines (Ця таблиця містить інформацію про різні лінії метро в місті)
+CREATE TABLE lines (
     id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) not null,
-    city_name varchar(255) not null, //Додано поле для city_name
+    city_name varchar(255) not null
 );
 
-//Таблиця  Stations (У цій таблиці містяться дані про різні станції метро в місті)
-                                                                            
-CREATE TABLE Stations (
+//Таблиця stations (У цій таблиці містяться дані про різні станції метро в місті)
+CREATE TABLE stations (
     id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) not null,
     line_id int,
+    prev_station_id INT DEFAULT NULL; //додав
+    next_station_id INT DEFAULT NULL; //додав
     latitude decimal(10, 8),
     longitude decimal(11, 8),
-    city_name varchar(255) not null, //Додано поле для city_name
-    FOREIGN KEY (line_id) REFERENCES Lines(id)
+    city_name varchar(255) not null,
+    FOREIGN KEY (line_id) REFERENCES lines(id)
 );
 
-// Таблиця Station Connections (У цій таблиці містяться дані про з'єднання між різними станціями в місті)
-                                                                          
-CREATE TABLE Station_Connections (
+
+
+//Таблиця transfers призначена для зберігання інформації про пересадки між різними станціями
+CREATE TABLE transfers( --додав
+id INT PRIMARY KEY,
+station_id_from INT DEFAULT NULL;
+station_id_to INT DEFAULT NULL;
+);
+
+
+
+
+
+//Таблиця station_Connections (У цій таблиці містяться дані про з'єднання між різними станціями в місті)
+CREATE TABLE station_connections (
     id int AUTO_INCREMENT PRIMARY KEY,
     station_id int,
     connected_station_id int,
-    order_number int, //Додано поле для визначення порядку
     connection_type varchar(255),
     city_name varchar(255) not null,//Додано поле для city_name
-    FOREIGN KEY (station_id) REFERENCES Stations(id),
-    FOREIGN KEY (connected_station_id) REFERENCES Stations(id)
+    FOREIGN KEY (station_id) REFERENCES stations(id),
+    FOREIGN KEY (connected_station_id) REFERENCES stations(id)
 );
 
-// Таблиця Branches (Ця таблиця містить дані про гілки метро в  місті)
-                                                                           
-CREATE TABLE Branches (
+// Таблиця branches (Ця таблиця містить дані про гілки метро в місті)
+CREATE TABLE branches (
     id int AUTO_INCREMENT PRIMARY KEY,
     name varchar(255) not null,
     line_id int,
-    FOREIGN KEY (line_id) REFERENCES Lines(id)
+    FOREIGN KEY (line_id) REFERENCES lines(id)
 );
 
 
-//Лінії визначають основні транспортні маршрути, пролягаючи через різні райони міста та створюючи каркас системи громадського транспорту,
- що об'єднує різні частини міста. Гілки є додатковими маршрутами, відгалуженнями від основних ліній, і призначені для обслуговування
-  конкретних районів чи станцій, маючи свої власні характеристики, але зазвичай підпорядковані та пов'язані з основною лінією.
+--Лінії визначають основні транспортні маршрути, пролягаючи через різні райони міста та створюючи каркас системи громадського транспорту,
+ --що об'єднує різні частини міста. Гілки є додатковими маршрутами, відгалуженнями від основних ліній, і призначені для обслуговування
+  --конкретних районів чи станцій, маючи свої власні характеристики, але зазвичай підпорядковані та пов'язані з основною лінією.
