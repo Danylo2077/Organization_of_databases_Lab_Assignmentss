edd6ea618b6f8e90bc4e73044048045d0a134d79
Danylo2077 <123510448+Danylo2077@users.noreply.github.com>
2023-11-24 21:49:15 +0200

    Add files via upload

--- Lab_8/Lab#8.sql
+++ Lab_8/Lab#8.sql
@@ -1,42 +1,68 @@
-//Тут можна зберігати дані про проекти
-CREATE TABLE Projects (
+//Таблиця для зберігання даних про проекти
+CREATE TABLE projects (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-//Тут можна зберігати дані про користувачів
-CREATE TABLE Users (
+//Таблиця для зберігання даних про користувачів
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(255) NOT NULL
 );
 
-//Тут можна зберігати дані про задачі
-CREATE TABLE Tasks (
+
+//Таблиця для прив'язки користувачів до проекту 
+CREATE TABLE project_members (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    project_id INT,
+    user_id INT,
+    PRIMARY KEY (project_id, user_id),
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
+
+//Таблиця для зберігання даних про задачі
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     project_id INT,
     author_id INT,
-    FOREIGN KEY (project_id) REFERENCES Projects(id),
-    FOREIGN KEY (author_id) REFERENCES Users(id)
+    FOREIGN KEY (project_id) REFERENCES projects(id),
+    FOREIGN KEY (author_id) REFERENCES users(id)
 );
 
-//Тут можна зберігати дані про виконавців завдань
-CREATE TABLE TaskAssignees (
-    assignee_id INT AUTO_INCREMENT PRIMARY KEY,
+//Таблиця для зберігання даних про виконавців завдань
+CREATE TABLE task_assignees (
     task_id INT,
     user_id INT,
-    FOREIGN KEY (task_id) REFERENCES Tasks(id),
-    FOREIGN KEY (user_id) REFERENCES Users(id)
+    PRIMARY KEY (task_id, user_id),
+    FOREIGN KEY (task_id) REFERENCES tasks(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
-//Тут можна зберігати дані про файли
-CREATE TABLE Files (
+
+//Таблиця для зберігання інформації про файли пов'язані з проектами
+CREATE TABLE project_files (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    file_url VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     project_id INT,
+    FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+
+
+
+//Таблиця для зберігання інформації про файли, пов'язані з задачами
+CREATE TABLE task_files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
     task_id INT,
-    FOREIGN KEY (project_id) REFERENCES Projects(id),
-    FOREIGN KEY (task_id) REFERENCES Tasks(id)
+    FOREIGN KEY (task_id) REFERENCES tasks(id)
 );
 
 
